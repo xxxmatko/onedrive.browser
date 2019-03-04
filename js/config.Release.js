0bf194b8ea538d5e415ed322c8f4ed618e66d867
xxxmatko <martin.sutka@gmail.com>
2019-03-04 09:09:48 +0100

    Config for app component.

--- js/config.Release.js
+++ js/config.Release.js
@@ -20,6 +20,16 @@ require({
             language: (window.location.href.indexOf("lang=") !== -1) ? window.location.href.split("lang=")[1].substr(0, 2) : document.documentElement.lang || "en"
         },
         "my/components/app/app": {
+            origin: "http://localhost:8021",
+            clientId: "0e68d12f-a492-487b-8916-4ff9f0493f37",
+            redirectUri: "http://localhost:8021/callback.html",
+            scopes: [
+                "user.read",
+                "files.read",
+                "files.read.all",
+                "sites.read.all"
+            ],
+            authServiceUri: "https://login.microsoftonline.com/common/oauth2/v2.0/authorize"            
         }
     }
 }, ["my/main"]);
